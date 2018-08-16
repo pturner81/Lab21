e1f51526178f95f1550902770d0a593fc5dd74ef
Turner <turnerp81@gmail.com>
2018-08-16 08:47:19 -0400

    Initial commit

--- Lab21/Controllers/HomeController.cs
+++ Lab21/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Lab21.Models;
 
 namespace Lab21.Controllers
 {
     public class HomeController : Controller
     {
+        List<UserInfo> users = new List<UserInfo>();
+
         public ActionResult Index()
         {
             return View();
@@ -23,6 +26,42 @@ namespace Lab21.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult RegistrationPage()
+        {
+            ViewBag.Message = "Register Here";
+
+            return View();
+        }
+        public ActionResult AddUser(UserInfo newUser)
+        {
+            List<UserInfo> users = new List<UserInfo>();
+            //NEEDS VALIDATION
+            if (ModelState.IsValid)
+            {
+                //NEEDS to send data to database
+                users.Add(newUser);
+
+                //NEEDS confirmation || send user to index page || sign them in etc.
+
+                ViewBag.ConfMessage = "Thanks " + newUser.FirstName + "!";
+
+                ViewBag.users = users;
+
+                return View();
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        public ActionResult Users()
+        {
+            ViewBag.Message = "Users";
+
+            ViewBag.users = users;
 
             return View();
         }
---
+++ Lab21/Models/UserInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab21.Models
+{
+    public class UserInfo
+    {
+        [Required] //attribute
+        [RegularExpression("^[a-zA-Z]{1,40}$")]
+        public string FirstName { set; get; }
+        [Required]
+        [RegularExpression("^[a-zA-Z]{1,40}$")]
+        public string LastName { set; get; }
+        [Required]
+        [RegularExpression("^[0-9]{10}$")]
+        public string PhoneNumber { set; get; }
+        [Required]
+        public string Email { set; get; }
+        [Required]
+        public string Password { set; get; }
+
+        //List<UserInfo> users = new List<UserInfo>();
+
+        public UserInfo()
+        {
+            FirstName = "First";
+            LastName = "Last";
+            PhoneNumber = "0";
+            Email = "email";
+            Password = "password";
+        }
+        public UserInfo(string fn, string ln, string pn, string em, string pw)
+        {
+            FirstName = fn;
+            LastName = ln;
+            PhoneNumber = pn;
+            Email = em;
+            Password = pw;
+        }
+
+    }
+}
